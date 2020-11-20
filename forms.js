58e3e53d9fbb5be0e51f521e4234f83d8e7ae37a
Nicolas Vera <57852440+queondatodotranqui@users.noreply.github.com>
2020-11-20 17:09:19 -0300

    validaciones

--- forms.js
+++ forms.js
@@ -1,5 +1,5 @@
 
-var btn = document.getElementById('submit');
+var formulario = document.getElementById('form');
 
 var nombre = document.getElementById('nombre');
 var edad = document.getElementById('edad');
@@ -8,28 +8,81 @@ var ciudad = document.getElementById('ciudad');
 var codigo = document.getElementById('codigo');
 var celular = document.getElementById('celular');
 
-function checkEmail(email){
-    var regEx = new regEx(/@/);
-    if (email.match(regEx) == -1){
-        return false;
-    }else{
-        return true;
+
+
+formulario.addEventListener('submit', (e)=>{
+
+    console.log('formulario enviado');
+    e.preventDefault();
+
+    if(validarCampos()){
+
     }
+
+})
+
+function checkEmail(email){
+    var reg = /@./;
+    return reg.test(email);
 }
+
+edad.addEventListener('keypress', (e)=>{
+    var caracter = e.charCode || e.keyCode;
+    if ((caracter < 48 || caracter > 57 ) || e.target.value.length > 1){
+        e.preventDefault();
+    }
+})
 
 function validarCampos(){
     var valido = true;
 
+    nombre.classList.remove('is-invalid', 'is-valid');
+    edad.classList.remove('is-invalid', 'is-valid');
+    email.classList.remove('is-invalid', 'is-valid');
+    ciudad.classList.remove('is-invalid', 'is-valid');
+    codigo.classList.remove('is-invalid', 'is-valid');
+    celular.classList.remove('is-invalid', 'is-valid');
+
     if(nombre.value == ''){
         nombre.classList.add('is-invalid');
         valido = false;
+    }else {
+        nombre.classList.add('is-valid');
     }
-    if(edad.value == '' && edad.value.length > 2){
+
+    if(edad.value == ''){
         edad.classList.add('is-invalid');
         valido = false;
+    }else{
+        edad.classList.add('is-valid');
     }
-    if(email.value == '' && checkEmail(email.value)){
+
+    if(email.value == '' && (!checkEmail(email.value))){
         email.classList.add('is-invalid');
         valido = false;
+    }else{
+        email.classList.add('is-valid');
     }
+
+    if(ciudad.value ==''){
+        ciudad.classList.add('is-invalid');
+        valido = false;
+    }else{
+        ciudad.classList.add('is-valid');
+    }
+
+    if(codigo.value ==''){
+        codigo.classList.add('is-invalid');
+        valido = false;
+    }else{
+        codigo.classList.add('is-valid');
+    }
+
+    if(celular.value ==''){
+        celular.classList.add('is-invalid');
+        valido = false;
+    }else{
+        celular.classList.add('is-valid');
+    }
+    return valido;
 }
